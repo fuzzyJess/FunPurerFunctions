6404c00b49cd26c1753db9e90093fc96e9df7b5a
fuzzyJess <jrundle99@yahoo.com>
2023-04-17 21:27:49 +0100

    add in test to check for returning a new array and not mutating the original array

--- __tests__/pure-test.js
+++ __tests__/pure-test.js
@@ -1,4 +1,4 @@
-const removeLastNumber = require('../pure.js')
+const {removeLastNumber, raiseSalaries} = require('../pure.js');
 
 describe('removeLastNumber', () => {
     test('returns a new array', () => {
@@ -16,3 +16,22 @@ describe('removeLastNumber', () => {
     expect(result).toEqual([1, 2, 3]);
   });
 });
+
+describe('raiseSalaries', () => {
+  test('returns a new array', () => {
+    const input = [{ name: "Alice", salary: 3000 }, 
+                  { name: "Bob", salary: 2000 }, 
+                  { name: "Vel", salary: 4500 }];
+    const returnValue = raiseSalaries(input, 10);
+    expect(input).not.toBe(returnValue);
+  });
+  test('does not mutate the input array', () => {
+    const input = [{ name: "Alice", salary: 3000 }, 
+                  { name: "Bob", salary: 2000 }, 
+                  { name: "Vel", salary: 4500 }];
+    raiseSalaries(input);
+    expect(input).toEqual([{ name: "Alice", salary: 3000 }, 
+                          { name: "Bob", salary: 2000 }, 
+                          { name: "Vel", salary: 4500 }]);
+  });
+})
--- pure.js
+++ pure.js
@@ -2,7 +2,29 @@ function removeLastNumber(numbers) {
     let beginningNumbers = [...numbers];
     beginningNumbers.pop();
 return beginningNumbers;
+};
+
+function raiseSalaries(employees, increase) {
+
 }
+// This function should take as its arguments:
 
+//an array representing a list of employees and 
+// a number (representing a percentage increase)
 
-module.exports = removeLastNumber;
+//each employee will be represented as an object with a name and a salary 
+//property.
+//It should return a new array of employees with their salary now increased 
+//by the given percentage increase. The new salaries should be rounded up 
+//to the nearest integer. None of the original employee objects should be 
+//mutated.
+
+/* raiseSalaries( [ { name: "Alice", salary: 3000 }, 
+                    { name: "Bob", salary: 2000 }, 
+                    { name: "Vel", salary: 4500 }, ], 10 ); */
+
+/* should return [  { name: 'Alice', salary: 3300 }, 
+                    { name: 'Bob', salary: 2200 }, 
+                    { name: 'Vel', salary: 4950 } ] */
+
+module.exports = {removeLastNumber, raiseSalaries};
